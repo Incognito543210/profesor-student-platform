0c80bf4a999a06f52394304caa653ed3c11a202f
MrTheoslav <wo303232@student.polsl.pl>
2024-05-12 20:46:23 +0200

    Added repository page

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,10 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from 'react';
 import { useAuth } from "../../Context/AuthContext";
 
 function HomePage() {
+  const [repositories, setRepositories] = useState([]);
+  const token = localStorage.getItem('token');
+
+  useEffect(() => {
+    if (!token) {
+      console.error('Token not found in local storage');
+      return;
+    }
+
+    fetch('https://localhost:7164/API/Repository/allRepository', {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, [token]);
+
+
   return (
     <div>
-      <h1>Witaj </h1>
+      <h1>Lista repozytoriów</h1>
+      <ul>
+        {repositories.map(repo => (
+          <li key={repo.id}>
+            <h2>{repo.topic}</h2>
+            <p>{repo.description}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
